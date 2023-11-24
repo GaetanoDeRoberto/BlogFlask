38291c2c41c3439e014717dc70304f591bd8924a
Gaetano De Roberto <gaetanoderoberto@gmail.com>
2023-11-24 17:52:07 +0100

    testo qui

--- blog.py
+++ blog.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template 
-
-#creazione istanza FLask
+from flask import Flask, render_template, flash 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired 
 
 app = Flask(__name__)
+app.config['SECRET_KEY']= "mia secret key"
+
+#creazione classe form 
+class NomeForm(FlaskForm):
+    nome = StringField("Inserisci il tuo nome", validators=[DataRequired()])
+    invia = SubmitField("Invia")
 
 #creazione route decoretor pagina index
 @app.route('/')
@@ -12,9 +19,9 @@ def index():
     return render_template('index.html', firstName=firstName, pizzePreferite=pizzePreferite)
     
 #pagina user
-@app.route('/user/<name>')
-def user(name):
-    return render_template("user.html", name=name )
+@app.route('/user/<nome>')
+def user(nome):
+    return render_template("user.html", nome=nome )
     #return "<h1>Hello {}!!!</h1>".format(name)
 
 #errore URL invalido
@@ -26,3 +33,17 @@ def paginaNonTrovata(e):
 @app.errorhandler(500)
 def paginaNonTrovata(e):
     return render_template('500.html'), 500
+
+@app.route('/nome', methods=['GET', 'POST'])
+def nome():
+    nome = None
+    form = NomeForm()
+    #Validazione form 
+    if form.validate_on_submit():
+        nome = form.nome.data
+        form.nome.data = ''
+        #messaggio da mostrare dopo il submit del form
+        flash("Nome inviato correttamente!")
+    return render_template('nome.html', nome=nome, form=form)
+
+
